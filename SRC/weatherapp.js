496639a932493727f808bb3cda00925fe4f6edb4
mcevans658 <145075206+mcevans658@users.noreply.github.com>
2023-09-22 14:27:55 -0500

    Updated JS
    fixed icon and auto populates Duluth now

--- SRC/weatherapp.js
+++ SRC/weatherapp.js
@@ -29,20 +29,25 @@ function displayWeather(response) {
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
-  document.querySelector("#description").innerHTML =
+  document.querySelector(".description").innerHTML =
     response.data.weather[0].description;
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/10d@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
-function search(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
+function search(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=842b36d55cb28eba74a018029d56b04c&units=imperial`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayWeather);
 }
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input");
+  search(city.value);
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
+
+search("Duluth");
